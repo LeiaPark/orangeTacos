3196c13b30df7bbfc6b135984e96247a927dd636
kimi <kimi@kimis-MacBook-Air.local>
2019-03-02 16:01:04 -0500

    asdfghjkl testing

---
+++ orangeTacos/HelloWorld.java
@@ -0,0 +1,20 @@
+/**************************************
+* File name: HelloWorld.java
+* Compilation: javac HelloWorld.java
+* Execution: java HelloWorld
+* 
+* Prints "hello, world". By tradition,
+* this is everyone's first program.
+***************************************/
+
+public class HelloWorld {
+	public static void main(String[] commandLine) {
+		System.out.println("hello, world");
+		System.out.println("bonjour, world"); // at home
+		System.out.println("salve, world"); // at StuyCS
+                System.out.println("ni hao, world"); // at homeS
+
+	}
+}
+
+ 
